ca91e4e59d24e3527bd5d2194b2dcf37a44b5eac
meghalawana1703@gmail.com <meghalawana1703@gmail.com>
2024-09-20 15:14:52 +0530

    Adding React files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App(){
+  return(
+    <div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, quas?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, velit.</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt, amet.</p>
+    </div>
+  )
+}
+
+export default App
